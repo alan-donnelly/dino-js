f3afa02ab1ae0f57d8be7a2058cd9eb2ea83011e
Alan Donnelly <alan_donnelly@mcafee.com>
2019-09-28 09:57:30 +0100

    Added week 1

--- dino.js
+++ dino.js
@@ -2,17 +2,16 @@ var thingsToLoad = [
     "dino-city.png"
 ];
 
-var g = hexi(480, 360, setup, thingsToLoad);
-g.backgroundColor = "white";
-g.start();
+var game = hexi(480, 360, setup, thingsToLoad);
+game.backgroundColor = "white";
+game.start();
 
 var dino;
 
 function setup() {
-    g.state = update;
+    game.state = update;
         
 }
-
 
 function update() {
 
